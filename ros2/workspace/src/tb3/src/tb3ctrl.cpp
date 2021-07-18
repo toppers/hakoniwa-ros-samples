b1c44759b7d71f04d3482bfc839b3ea9b82e0758
tmori <tmori@esm.co.jp>
2021-07-18 13:42:45 +0900

    format cpp

--- ros2/workspace/src/tb3/src/tb3ctrl.cpp
+++ ros2/workspace/src/tb3/src/tb3ctrl.cpp
@@ -1,83 +1,76 @@
-#include "rclcpp/rclcpp.hpp"
-#include "sensor_msgs/msg/laser_scan.hpp"
-#include "sensor_msgs/msg/imu.hpp"
-#include "geometry_msgs/msg/twist.hpp"
 #include <stdio.h>
 
+#include "geometry_msgs/msg/twist.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/imu.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
+
 typedef struct {
-	double ranges[360];
+  double ranges[360];
 } ScanDataType;
 
 static ScanDataType scan_data;
 
-static void scanCallback(const sensor_msgs::msg::LaserScan::SharedPtr msg)
-{
-
-	int i;
-	for (i = 0; i < 360; i++) {
-		scan_data.ranges[i] = msg->ranges[i];
-	}
-	return;
+static void scanCallback(const sensor_msgs::msg::LaserScan::SharedPtr msg) {
+  int i;
+  for (i = 0; i < 360; i++) {
+    scan_data.ranges[i] = msg->ranges[i];
+  }
+  return;
 }
 
 static geometry_msgs::msg::Twist cmd_vel;
 
-static float get_foward_distance(void)
-{
-	int i;
-	float min = 100.0f;
-	for (i = 0; i < 15; i++) {
-		if (scan_data.ranges[i] < min) {
-			min = scan_data.ranges[i];
-		}
-	}
-	for (i = (360 - 15); i < 360 ; i++) {
-		if (scan_data.ranges[i] < min) {
-			min = scan_data.ranges[i];
-		}
-	}
-	//printf("foward: %lf\n", min);
-	return min;
+static float get_foward_distance(void) {
+  int i;
+  float min = 100.0f;
+  for (i = 0; i < 15; i++) {
+    if (scan_data.ranges[i] < min) {
+      min = scan_data.ranges[i];
+    }
+  }
+  for (i = (360 - 15); i < 360; i++) {
+    if (scan_data.ranges[i] < min) {
+      min = scan_data.ranges[i];
+    }
+  }
+  // printf("foward: %lf\n", min);
+  return min;
 }
-static float get_right_distance(void)
-{
-	int i;
-	float min = 100.0f;
-	for (i = (90 - 30); i < (90 + 30); i++) {
-		if (scan_data.ranges[i] < min) {
-			min = scan_data.ranges[i];
-		}
-	}
-	//printf("right: %lf\n", min);
-	return min;
+static float get_right_distance(void) {
+  int i;
+  float min = 100.0f;
+  for (i = (90 - 30); i < (90 + 30); i++) {
+    if (scan_data.ranges[i] < min) {
+      min = scan_data.ranges[i];
+    }
+  }
+  // printf("right: %lf\n", min);
+  return min;
 }
-static float sarch_all(void)
-{
-	int i;
-	float min = 100.0f;
-	for (i = 0; i < 360; i++) {
-		if (scan_data.ranges[i] < min) {
-			min = scan_data.ranges[i];
-		}
-	}
-	return min;
+static float sarch_all(void) {
+  int i;
+  float min = 100.0f;
+  for (i = 0; i < 360; i++) {
+    if (scan_data.ranges[i] < min) {
+      min = scan_data.ranges[i];
+    }
+  }
+  return min;
 }
 
-static bool do_foward(void)
-{
-	bool is_stop = false;
-	cmd_vel.linear.x = 0;
-	if (get_foward_distance() < 0.2f) {
-		cmd_vel.linear.x = 0;
-		is_stop = true;
-	}
-	else {
-		cmd_vel.linear.x = 2;
-	}
-	
-	return is_stop;
-}
+static bool do_foward(void) {
+  bool is_stop = false;
+  cmd_vel.linear.x = 0;
+  if (get_foward_distance() < 0.2f) {
+    cmd_vel.linear.x = 0;
+    is_stop = true;
+  } else {
+    cmd_vel.linear.x = 2;
+  }
 
+  return is_stop;
+}
 
 #if 0
 static bool turn_left(void)
@@ -96,68 +89,62 @@ static bool turn_left(void)
 }
 #endif
 
-static bool turn_right(void)
-{
-	bool is_stop = false;
-	cmd_vel.angular.z = 0;
-	if (get_right_distance() < 0.05f) {
-		cmd_vel.angular.z = -5;
-		is_stop = true;
-	}
-	else {
-		cmd_vel.angular.z = 0;
-	}
-	
-	return is_stop;
+static bool turn_right(void) {
+  bool is_stop = false;
+  cmd_vel.angular.z = 0;
+  if (get_right_distance() < 0.05f) {
+    cmd_vel.angular.z = -5;
+    is_stop = true;
+  } else {
+    cmd_vel.angular.z = 0;
+  }
+
+  return is_stop;
 }
 
-static void do_control(void)
-{
-	
-	(void)do_foward();
-	(void)turn_right();
-	
-	if (cmd_vel.linear.x == 0 && cmd_vel.angular.z == 0) {
-		cmd_vel.angular.z = 1;
-	}
-	return;
+static void do_control(void) {
+  (void)do_foward();
+  (void)turn_right();
+
+  if (cmd_vel.linear.x == 0 && cmd_vel.angular.z == 0) {
+    cmd_vel.angular.z = 1;
+  }
+  return;
 }
 using namespace std::chrono_literals;
 
 typedef enum {
-	RoboMode_INIT = 0,
-	RoboMode_RUN,
+  RoboMode_INIT = 0,
+  RoboMode_RUN,
 } RoboModeType;
-int main(int argc, char** argv)
-{
-	RoboModeType mode = RoboMode_INIT;
+int main(int argc, char **argv) {
+  RoboModeType mode = RoboMode_INIT;
 
-	rclcpp::init(argc, argv);
-	
-	auto node = rclcpp::Node::make_shared("tb3_node");
-	auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 1);
-	auto subscriber = node->create_subscription<sensor_msgs::msg::LaserScan>("scan", 1, scanCallback);
+  rclcpp::init(argc, argv);
 
-	rclcpp::WallRate rate(100ms);
+  auto node = rclcpp::Node::make_shared("tb3_node");
+  auto publisher =
+      node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 1);
+  auto subscriber = node->create_subscription<sensor_msgs::msg::LaserScan>(
+      "scan", 1, scanCallback);
 
-    while (rclcpp::ok())
-    {
-    	if (mode == RoboMode_INIT) {
-    		float d = sarch_all();
-    		if (d > 0.0f && d <= 0.08f) {
-    			printf("d=%f MOVE\n", d);
-    			mode = RoboMode_RUN;
-    		}
-    		else {
-    			printf("WATING d=%f\n", d);
-    		}
-    	}
-    	else {
-	    	do_control(); 
-	    	publisher->publish(cmd_vel);
-    	}
-    	rclcpp::spin_some(node);
-    	rate.sleep();
+  rclcpp::WallRate rate(100ms);
+
+  while (rclcpp::ok()) {
+    if (mode == RoboMode_INIT) {
+      float d = sarch_all();
+      if (d > 0.0f && d <= 0.08f) {
+        printf("d=%f MOVE\n", d);
+        mode = RoboMode_RUN;
+      } else {
+        printf("WATING d=%f\n", d);
+      }
+    } else {
+      do_control();
+      publisher->publish(cmd_vel);
     }
-    return 0;
+    rclcpp::spin_some(node);
+    rate.sleep();
+  }
+  return 0;
 }
