96b7e05d251638699aa6c0ca9294ffd99bf5c9c1
Takashi Mori <tmori@esm.co.jp>
2021-07-21 13:11:54 +0900

    add pdu_configs

--- utils/core_config/all.bash
+++ utils/core_config/all.bash
@@ -3,10 +3,13 @@
 CORE_IPADDR=127.0.0.1
 ROS_IPADDR=172.21.68.89
 ROS_TOPIC_FILE=../../settings/ev3_tb3/RosTopics.json
+ROS_TOPIC_FILE_PATH=../../../settings/ev3_tb3/RosTopics.json
 OUT_DIR=../../ros2/unity/tb3_ros2/
 PKG_NAME=EV3_TB3
+PDU_CONFIG_INDIR=./ros_json
+ROS_MSG_LIST=../all_msgs.txt
 
-python create_core_config.py		${ROS_TOPIC_FILE} 	${CORE_IPADDR}  ${OUT_DIR}
+python create_core_config.py		${ROS_TOPIC_FILE_PATH} 	${CORE_IPADDR}  ${OUT_DIR}
 python create_ros_topic_method.py	${PKG_NAME}			${OUT_DIR}
 python create_inside_assets.py  	${ROS_TOPIC_FILE} 	${OUT_DIR}
 python create_pdu_rw.py  			${ROS_TOPIC_FILE} 	${OUT_DIR} ${PKG_NAME} r
@@ -15,3 +18,4 @@ python create_connector_rw.py  		${ROS_TOPIC_FILE} 	${OUT_DIR} r
 python create_connector_rw.py  		${ROS_TOPIC_FILE} 	${OUT_DIR} w
 python create_connector_rw.py  		${ROS_TOPIC_FILE} 	${OUT_DIR} p
 python create_unity_ros_params.py 	${ROS_IPADDR}		${OUT_DIR}
+python create_pdu_config.py			${PDU_CONFIG_INDIR}	${ROS_MSG_LIST}	${OUT_DIR}
---
+++ utils/core_config/create_pdu_config.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import json
+import sys
+import glob
+import re
+from collections import OrderedDict
+
+if len(sys.argv) != 4:
+	print "Usage: " + sys.argv[0] + " <in_dir> <ros_msg_list> <out_dir>"
+	sys.exit()
+
+in_dir=sys.argv[1]
+in_file=sys.argv[2]
+out_dir=sys.argv[3]
+out_filename='pdu_configs.json'
+
+container = list()
+for pkg_msg in open(in_file, 'r'):
+    entry = OrderedDict()
+    entry['pdu_type_name'] = pkg_msg.strip()
+    entry['pdu_data_field_path'] = in_dir + '/' + pkg_msg.strip() + '.json'
+    container.append(entry)
+    entry = OrderedDict()
+    entry['pdu_type_name'] = pkg_msg.strip().split('/')[1]
+    entry['pdu_data_field_path'] = in_dir + '/' + pkg_msg.strip() + '.json'
+    container.append(entry)
+    
+
+with open(out_dir + '/' + out_filename, mode='wt') as out_file:
+  json.dump(container, out_file, ensure_ascii=False, indent=2)
+
+
