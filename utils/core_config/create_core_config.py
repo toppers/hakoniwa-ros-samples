948bc917af2c975ba1eb1365da5c269f8937b43a
Tatsuya Fukuta <fukuta_tatsuya@intec.co.jp>
2021-10-08 10:53:24 +0900

    Update the configuration - Added support for automatic generation of world_param.json - Adjusted the frequency of publishing some sensor data. - Refactored all.bash to follow ShellCheck.

--- utils/core_config/create_core_config.py
+++ utils/core_config/create_core_config.py
@@ -32,6 +32,7 @@ out_data['mmap_methods_path'] = './mmap_methods.json'
 out_data['reader_connectors_path'] = './reader_connectors.json'
 out_data['writer_connectors_path'] = './writer_connectors.json'
 out_data['pdu_channel_connectors_path'] = './pdu_channel_connectors.json'
+out_data['param_world_config_path'] = './param_world.json'
 
 with open(out_dir + '/core_config.json', mode='wt') as out_file:
   json.dump(out_data, out_file, ensure_ascii=False, indent=2)
