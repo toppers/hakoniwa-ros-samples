a0295f18174112a987336bed6c221cb4461c625c
Takashi Mori <kanetugu2015@gmail.com>
2022-03-20 21:19:15 +0900

    add linux env variable for docker run

--- ros2/docker/run-linux.bash
+++ ros2/docker/run-linux.bash
@@ -8,4 +8,5 @@ DOCKER_IMAGE=${DOCKER_IMAGE}:v1.0.0
 docker run -v ${HAKONIWA_TOP_DIR}:/root/workspace/hakoniwa-ros-samples \
 		-e CORE_IPADDR=127.0.0.1 \
 		-e ROS_IPADDR=127.0.0.1 \
+		-e OS_TYPE="Linux" \
 		-it --rm --net host --name hakoniwa-ros-sim ${DOCKER_IMAGE} 
